ee4ea6ebe4eb2837daaa3aae594f1203e95e9b02
hyulevo653 <duchuy653201@gmail.com>
2022-08-30 10:49:56 +0700

    Add to Cart Function

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
     products: [
         {
+            _id: '1',
             name: 'Nike Slim shirt',
             slug: 'nike-slim-shirt',
             category:'Shirts',
@@ -13,6 +14,7 @@ const data = {
             description: 'high quality shirt',
         },
         {
+            _id: '2',
             name: 'Adidas Fit Shirt',
             slug: 'adidas-fit-shirt',
             category:'Shirts',
@@ -24,7 +26,8 @@ const data = {
             numReviews: 10,
             description: 'high quality shirt',
         },
-        {
+        {   
+            _id: '3',
             name: 'Nike Slim Pant',
             slug: 'nike-slim-pant',
             category:'Pants',
@@ -36,7 +39,8 @@ const data = {
             numReviews: 10,
             description: 'high quality shirt',
         },
-        {
+        {   
+            _id: '4',
             name: 'Nike Fit Pant',
             slug: 'nike-fit-pant',
             category:'Pants',
