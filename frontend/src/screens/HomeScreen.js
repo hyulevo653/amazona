10ad9afc61b38bdfe6a2831e38fee32113a1a13d
hyulevo653 <duchuy653201@gmail.com>
2022-08-30 10:20:47 +0700

    create react context

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,6 +6,9 @@ import { useEffect, useReducer, useState } from "react";
 import axios from 'axios'
 import logger from 'use-reducer-logger';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -48,9 +51,9 @@ function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <div>Loading...</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row> {
           products.map((product) => (
