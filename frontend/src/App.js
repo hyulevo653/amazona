bf689cfe460c2ad71197b3734409db516cc8d654
hyulevo653 <duchuy653201@gmail.com>
2022-09-12 22:29:16 +0700

    Signup Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import { Store } from './screens/Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import SignupScreen from './screens/SignupScreen';
 
 
 
@@ -80,6 +81,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen/>} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route
                 path="/shipping"
                 element={<ShippingAddressScreen />}
