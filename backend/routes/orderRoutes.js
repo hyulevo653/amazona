278df67e54ca0a08b46e28e225b74ee08a8ebb39
hyulevo653 <duchuy653201@gmail.com>
2022-09-26 21:53:26 +0700

    Deliver Order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -82,23 +82,18 @@ orderRouter.get(
   })
 );
 
+
+
 orderRouter.put(
-  '/:id/pay',
+  '/:id/deliver',
   isAuth,
   expressAsyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id);
     if (order) {
-      order.isPaid = true;
-      order.paidAt = Date.now();
-      order.paymentResult = {
-        id: req.body.id,
-        status: req.body.status,
-        update_time: req.body.update_time,
-        email_address: req.body.email_address,
-      };
-
-      const updatedOrder = await order.save();
-      res.send({ message: 'Order Paid', order: updatedOrder });
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+      await order.save();
+      res.send({ message: 'Order Delivered' });
     } else {
       res.status(404).send({ message: 'Order Not Found' });
     }
