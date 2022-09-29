dc02d3196f87e6087b832209a21f67f1c17c87d7
hyulevo653 <duchuy653201@gmail.com>
2022-09-29 21:45:36 +0700

    UserEditScreen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,6 +1,8 @@
-import React, { useReducer,useContext,useEffect } from 'react';
 import axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { Store } from './Store';
@@ -26,6 +28,7 @@ const reducer = (state, action) => {
 
 
   export default function UserListScreen() {
+    const navigate = useNavigate();
     const [{ loading, error, users }, dispatch] = useReducer(reducer, {
       loading: true,
       error: '',
@@ -79,7 +82,15 @@ const reducer = (state, action) => {
                   <td>{user.name}</td>
                   <td>{user.email}</td>
                   <td>{user.isAdmin ? 'YES' : 'NO'}</td>
-                  <td></td>
+                  <td>
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={() => navigate(`/admin/user/${user._id}`)}
+                  >
+                    Edit
+                  </Button>
+                </td>            
                 </tr>
               ))}
             </tbody>
